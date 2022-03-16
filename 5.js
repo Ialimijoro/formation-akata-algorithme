4878306d78602ec9c516a84d5f293852061a2e4a
minancy00 <miaryakata@gmail.com>
2022-03-16 10:05:08 +0300

    fonction afficherAlEnvers

--- 5.js
+++ 5.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function afficherAlEnvers() {
-  
+function afficherAlEnvers(liste) {
+  var tailleliste = liste.length;
+  var resultat = []
+  for (var i = tailleliste-1; i >= 0; i--){
+    resultat += liste[i];
+  }
+  return resultat;
 }
+console.log(afficherAlEnvers([0, 1, 2, 3, 4]));
