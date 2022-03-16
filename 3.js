7b7afd3a599361e80036ea9e57d3d7d73e438824
minancy00 <miaryakata@gmail.com>
2022-03-16 10:01:09 +0300

    fonction nombrePair

--- 3.js
+++ 3.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function nombrePair() {
-  
+function nombrePair(valeur) {
+  if (valeur % 2 == 0) {
+    console.log("Le nombre est pair");
+    return true;
+  }
+  console.log("Le nombre est impair");
+  return false;
 }
+console.log(nombrePair(7));
