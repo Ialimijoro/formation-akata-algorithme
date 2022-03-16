606f9d5b9c620de23e3cdc90508ce417f6980987
minancy00 <miaryakata@gmail.com>
2022-03-16 10:11:35 +0300

    fonction maximum

--- 10.js
+++ 10.js
@@ -1,4 +1,12 @@
 // veuillez complétez la fonction, ainsi que son argument
-function maximum() {
-  
+function maximum(tableau) {
+  var tailletableau = tableau.length;
+    var max = tableau[0];
+    for (var i = 1; i <= tailletableau; i++){
+      if(tableau[i]> max){
+        max = tableau [i];
+      }
+    }
+    return max;
 }
+console.log(maximum([0, 1, 77, 3, 4]));
