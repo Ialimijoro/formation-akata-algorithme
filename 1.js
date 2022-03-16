5f385fec3249cf08edd71aa68727efd5c02874b1
minancy00 <miaryakata@gmail.com>
2022-03-16 09:53:27 +0300

    fonction racineCarre

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 // veuillez complétez la fonction, ainsi que son argument
-function racineCarre() {
+function racineCarre(nombre) {
+  var resultat = 1, i = 0, trouve = false;
+  
+  while (!trouve) {
+    i = i + 1;
+    resultat = (nombre / resultat + resultat)/2;
+    if (i == nombre  + 1) {
+      break;
+    }
+  }
+  return resultat;
   
 }
+console.log(racineCarre(49))
