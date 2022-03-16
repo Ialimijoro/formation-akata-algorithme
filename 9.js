d71e3b6254b192b71dd9b9d675acb1a8517ee8e9
minancy00 <miaryakata@gmail.com>
2022-03-16 10:09:48 +0300

    fonction minimum

--- 9.js
+++ 9.js
@@ -1,4 +1,12 @@
 // veuillez complétez la fonction, ainsi que son argument
-function minimum() {
-  
+function minimum(tableau) {
+  var tailletableau = tableau.length;
+    var min = tableau[0];
+    for (var i = 1; i <= tailletableau; i++){
+      if(tableau[i]< min){
+        min = tableau [i];
+      }
+    }
+    return min;
 }
+console.log(minimum([-9, 1, -77, 3, 4]));
