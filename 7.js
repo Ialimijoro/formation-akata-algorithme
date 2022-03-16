863fe6b4b3f15a3350ef024221bb0a7546c23cb9
minancy00 <miaryakata@gmail.com>
2022-03-16 10:07:36 +0300

    fonction retournerPomme

--- 7.js
+++ 7.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function retournerPomme() {
-  
+var hash = {
+  pomme: 10,
+  banane: 2,
+};
+function retournerPomme(hash) {
+  return hash.pomme;
 }
+
+console.log(retournerPomme(hash));
