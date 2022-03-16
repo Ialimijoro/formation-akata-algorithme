2c5a08dbfac85bd3673c4598d452f5e57739d995
minancy00 <miaryakata@gmail.com>
2022-03-16 10:06:30 +0300

    fonction renverserChaineDeCaractere

--- 6.js
+++ 6.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function renverserChaineDeCaractere() {
-  
+function renverserChaineDeCaractere(chaine) {
+  var taillechaine = chaine.length;
+  var resultat="";
+  for(var i = taillechaine-1; i >= 0; i--){
+    resultat += chaine[i];
+  }
+  return resultat;
 }
+console.log(renverserChaineDeCaractere("nancy"))
