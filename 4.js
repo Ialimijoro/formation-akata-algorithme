78ec480da3e4717647f51abb6d7860d67fb9cafb
minancy00 <miaryakata@gmail.com>
2022-03-16 10:02:14 +0300

    fonction nombrePremier

--- 4.js
+++ 4.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function nombrePremier() {
-  
+function nombrePremier(nombre) {
+  for (var i = 2; i <= nombre/2; i++) {
+    if (nombre % i == 0) {
+      return false;
+    }
+  }
+  return nombre > 1;
 }
+console.log(nombrePremier(14));
