619458cc9a74d3de634dd89d07f247b41ef52332
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:33:50 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -1,4 +1,14 @@
 // veuillez complétez la fonction, ainsi que son argument
-function maximum() {
-  
-}
+function maximum(tableau) {
+        var maxi = tableau[0];
+        for (i = 0; i < tableau.length; i++) {
+          if (tableau[i] > maxi) {
+            maxi = tableau[i];
+          }
+        }
+      
+        return maxi;
+      }
+      
+      console.log(maximum([0,46,79,102,-77]));
+
