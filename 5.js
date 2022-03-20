03df28cbb441d8781947a89b8e6f3720fbfb6902
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:37:09 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,4 +1,17 @@
 // veuillez complétez la fonction, ainsi que son argument
-function afficherAlEnvers() {
-  
+function afficherAlEnvers(liste) {
+    liste = [1,2,3,4,5,6,7,8,9,10],
+    liste_inverse = []; 
+
+nbr=0
+
+for(i=0;i<liste.length;i++){
+    nbr=(liste.length-i)-1
+    liste_inverse[i]=liste[nbr]
 }
+
+liste= liste_inverse; 
+
+console.log(liste)
+}
+console.log(afficherAlEnvers([1,2,3,4,5,6,7,8,9,10]))
