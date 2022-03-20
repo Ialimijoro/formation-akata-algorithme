8a33f1c0fb25c678139cfdcb19e71e0e667af671
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:39:01 +0300

    Update 8.js

--- 8.js
+++ 8.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
 function changerAkata() {
-  
+  var hashage={
+      akata : 1,
+      autrecle : 2,
+  }
+  hashage.akata = "goavana";
+  return hashage.akata;
 }
+console.log(changerAkata())
