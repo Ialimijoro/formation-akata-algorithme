fd55e9560401dec998240188f34fa6e0f8987008
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:39:37 +0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,4 +1,13 @@
 // veuillez complétez la fonction, ainsi que son argument
-function minimum() {
+function minimum(tableau) {
+    var mini = tableau[0];
+    for (i = 0; i < tableau.length; i++) {
+      if (tableau[i] < mini) {
+        mini = tableau[i];
+      }
+    }
   
-}
+    return mini;
+  }
+  
+  console.log(minimum([87,46,79,102,-77]));
