461181d4144e699a3ea6c90a5dfe8ca5cc1faf17
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:36:18 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,4 +1,11 @@
 // veuillez complétez la fonction, ainsi que son argument
-function nombrePremier() {
-  
+function nombrePremier(nbr) {
+    for(var i = 2; i < nbr; i++)
+    if(nbr%i == 0){
+     return false;
+    }
+
+  return nbr > 1;
 }
+console.log(nombrePremier(31));
+
