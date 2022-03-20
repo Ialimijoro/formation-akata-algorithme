ba383a7f44201e9bcab4413eb1a1bf25ac172b58
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:31:08 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,4 +1,10 @@
 // veuillez complétez la fonction, ainsi que son argument
-function factoriel() {
-  
+function factoriel(nbr) 
+{
+  if (nbr === 0)
+  {
+     return 1;
+  }
+  return nbr * factoriel(nbr-1);
 }
+console.log(factoriel(5));
