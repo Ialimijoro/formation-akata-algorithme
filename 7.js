8622eacc766c2a7bfa853a96557613364f1649b4
mandresy889 <98636657+mandresy889@users.noreply.github.com>
2022-03-20 04:38:26 +0300

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,4 +1,13 @@
 // veuillez complétez la fonction, ainsi que son argument
-function retournerPomme() {
-  
+function retournerPomme(hashage) {
+  var hashage ={
+      pomme : 15,
+      ananas : 2,
+      banane : 20
+
+  }
+  return hashage.pomme;
+
 }
+
+console.log(retournerPomme());
